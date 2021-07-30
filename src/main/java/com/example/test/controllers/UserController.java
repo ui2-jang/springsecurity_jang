bf3ebe23ebcfe8c2b2c692d18198586d7a8c917c
Jang <jang@ui2.co.jp>
2021-07-30 15:54:41 +0900

    4. validation error message 追加と修正

--- src/main/java/com/example/test/controllers/UserController.java
+++ src/main/java/com/example/test/controllers/UserController.java
@@ -6,14 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+import java.util.concurrent.atomic.AtomicReference;
+
 @Controller
 public class UserController {
+
+    @Autowired
+    HttpSession session;
 
     @Autowired
     UserService userService;
@@ -26,8 +33,15 @@ public class UserController {
     @PostMapping("/signup_")
     public String signup(@Validated UserForm userForm,
                          BindingResult bindingResult,
-                         @RequestParam("confirm_password") String confirm) {
+                         @RequestParam("confirm_password") String confirm,
+                         Model model) {
+            String message = "";
             if(bindingResult.hasErrors()) {
+//                bindingResult.getAllErrors().forEach(c -> {
+//                    System.out.println(c.getDefaultMessage());
+//                });
+                message = bindingResult.getAllErrors().get(0).getDefaultMessage();
+                session.setAttribute("message",message);
                 return "redirect:/signup";
             } else {
                 if(userForm.getPassword().equals(confirm)) {
--- src/main/java/com/example/test/form/UserForm.java
+++ src/main/java/com/example/test/form/UserForm.java
@@ -5,13 +5,17 @@ import lombok.Data;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class UserForm {
     private int userid;
-    @NotEmpty
-    @Email
+    @NotEmpty(message = "必須項目です。")
+    @Email(message = "UserのIDはEmailの型で入力して下さい。")
     private String username;
-    @NotEmpty
-    @Min(6)
+    @NotEmpty(message = "必須項目です。")
+//    @Min(6,message = "パスワードは最小6文字です。")
+//    private String password;
+    @Size(min=6,message = "パスワードは最小6文字です。")
     private String password;
 }
